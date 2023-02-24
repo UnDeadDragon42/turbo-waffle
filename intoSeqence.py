75976b9150c11f4762f677bb063646b4fd91ea95
UnDeadDragon42 <jdwinther11@gmail.com>
2023-02-24 12:15:26 -0700

    asd

--- intoSeqence.py
+++ intoSeqence.py
@@ -1,6 +1,4 @@
 import time
 def main():
 	print('''\nFinaly after all this time, after all the deception, trickery, and wrong moves you are here. King Movieses tomb
-In front of you stands a large stone door without any handles just a hole on the right door. The steps in front of the door is covered in sand.
-
-What do you do?''')
+In front of you stands a large stone door without any handles just a hole on the right door. The steps in front of the door is covered in sand.''')
--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import userActions
 #print("HERE WE ARE")
 intoSeqence.main()
 
-userIn = input("What do you do?")
+userIn = input("What do you do?\n")
 
 if userIn == "help":
 	userActions.printActions()
+else:
+	print("Type 'help' for list of commands")
