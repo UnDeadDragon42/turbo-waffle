3fb47c2666d9bbfa6b8adfdeef40581cdc8e260f
UnDeadDragon42 <jdwinther11@gmail.com>
2023-02-24 12:13:28 -0700

    added help user action

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import intoSeqence
+import userActions
 #def main():
 #print("HERE WE ARE")
 intoSeqence.main()
+
+userIn = input("What do you do?")
+
+if userIn == "help":
+	userActions.printActions()
---
+++ userActions.py
@@ -0,0 +1,7 @@
+
+def printActions():
+	print(''''search'- looks in imdeiat area for iteams, 'help' - lists posible commands, 
+'examine _____' - looks closly at object to give hints, 'iteams in room' - lists intractible objects in the room
+'inventroy' - lists inventory, 'use ___ on ___' - uses object on iteam
+	
+	''')
