4a088d035181c56f4645c36c1ac52e314a5df188
UnDeadDragon42 <jdwinther11@gmail.com>
2023-04-14 12:28:56 -0600

    added rooms that need to be finsihed

--- rooms.py
+++ rooms.py
@@ -2,26 +2,37 @@ rooms = ["outside", "entrance"]
 playersLokation = "outside"
 
 class Rooms():
-	def __init__(self):
-		self.name = "room"
-		self.number = 0
+	def __init__(self, name = 'room', number = 0):
+		self.name = name
+		self.number = number
 		self.directions = {}
 		self.objects = []
 		self.objectDesriptions = []
 		self.roomdescriptions = {}
 		self.hints = []
 
-outside = Rooms()
-outside.name = "outside"
-outside.number = 1
+outside = Rooms("outside", 1)
 outside.directions = "forward"
 outside.objects = "key"
 outside.objectDesriptions = "Rusty circular key, it looks like it has been covered up for a very long time"
 outside.roomdescriptions = {"You stand befor the tome of King Movieses. In front of you stands a door with an oddly shapped key hole, with the entrance steps covered in sand.", "You stand befor the tome of King Movieses, the door way now uncovered. The door is open now leading into darkness that has not been seen for centruies"}
 outside.hints = ["I wonder where the key could be?", "There is a lot of sand", "If only I could move it out of the way."]
 
-mainHall = Rooms()
-mainHall.name = "main hall"
-mainHall.number = 2
-mainHall.directions = {"forward", "backward"}
+mainHall = Rooms("main hall", 2)
+mainHall.directions = {"east", "backward", "west"}
 mainHall.roomdescriptions = {"You stand in the main hall of the tomb. From here WRITE THE DIRECTIONS"}
+
+watingR = Rooms("wating room", 3)
+watingR.directions = {"north", "south"}
+
+dinnH = Rooms("dinning hall", 4)
+dinnH.directions{"north", "east", "south"}
+
+hiddenR = Rooms("hidden room", 5)
+hiddenR.directions("north")
+
+obserR = Rooms("observertory", 6)
+
+waterR = Rooms("water room", 7)
+
+tomb = Rooms("tomb", 8)
